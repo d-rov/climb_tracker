b0334a8d6f0b1e1f913f8d992134d64a7c4545b2
d-rov <duncan.rov@gmail.com>
2021-11-01 18:40:34 -0600

    working on figuring out how to verify user login

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const client = new MongoClient(uri)
 
 app.use(express.json())
 
+// helper function for GET one by name
+// might make sense to merge with the GET call
 async function findClimb(client, nameOfClimb) {
     try {
         await client.connect()
         const result = await client.db("climbing_tracker").collection("climbs").findOne({ name: nameOfClimb })
-
         if (result) {
             return result
         } else {
             console.log("no results")
         }
-
     } finally {
         client.close()
     }
@@ -37,6 +37,7 @@ app.get('/express_backend', (req, res) => {
     res.send({ express: "YOUR EXPRESS BACKEND IS UP AND RUNNING" })
 })
 
+// climb GET one by name
 app.get('/names/', async (req, res) => {
     const climb = req.query.name
     const message = await findClimb(client, climb).catch(console.error)
@@ -44,6 +45,7 @@ app.get('/names/', async (req, res) => {
     res.send(message)
 })
 
+// climb POST
 app.post('/new_climb/', async (req, res) => {
     try {
         const newClimb = req.body
@@ -59,9 +61,12 @@ app.post('/new_climb/', async (req, res) => {
 app.get('/users/', async(req, res) => {
     try {
         // connect
+        const user = req.query.username
         await client.connect()
         // findOne
+        const profile = await client.db("climbing_tracker").collection("users").findOne({ username: user })
         // return result or lack thereof
+        res.send(profile)
     } finally {
         client.close()
     }
@@ -71,6 +76,8 @@ app.get('/users/', async(req, res) => {
 // climbs GET all by user
 // climbs GET all in progress by user
 // climbs GET all completed by user
+// POST climb to users in progress field
+// POST climb to users completed field
 
 /*
 user = {
